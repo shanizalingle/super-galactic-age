236297d3b361eefd7ad3533a54e81f1ed0ab7530
Shaniza Lingle <shanizalingle@gmail.com>
2022-09-02 11:46:52 -0700

    pass SuperGalacticAge() test 2

--- src/js/business.js
+++ src/js/business.js
@@ -1,5 +1,14 @@
-export default class Age {
-  constructor(earthAge) {
+export default class SuperGalacticAge {
+  constructor(earthAge, mercuryAge) {
     this.earthAge = earthAge;
+    this.mercuryAge = mercuryAge;
+  }
+
+  findMercuryAge() {
+    let newMercuryAge;
+    newMercuryAge = this.earthAge * .24;
+    this.mercuryAge = newMercuryAge;
+    return newMercuryAge;
   }
 }
+
