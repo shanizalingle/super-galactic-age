e1c770d51c52c35c1ccc5e5bb885cd5c886a9635
Shaniza Lingle <shanizalingle@gmail.com>
2022-09-02 14:35:14 -0700

    pass SuperGalacticAge() test 6

--- src/js/business.js
+++ src/js/business.js
@@ -1,12 +1,14 @@
 export default class SuperGalacticAge {
-  constructor(earthAge, mercuryAge, venusAge, marsAge, jupiterAge) {
+  constructor(earthAge, mercuryAge, venusAge, marsAge, jupiterAge, earthLifeSpan) {
     this.earthAge = earthAge;
     this.mercuryAge = mercuryAge;
     this.venusAge = venusAge;
     this.marsAge = marsAge;
     this.jupiterAge = jupiterAge;
+    this.earthLifeSpan = earthLifeSpan;
   }
 
+// Determine age on other planets
   findMercuryAge() {
     let newMercuryAge;
     newMercuryAge =Math.round((this.earthAge * .24) * 100)/100;;
@@ -33,6 +35,21 @@ export default class SuperGalacticAge {
     newJupiterAge = Math.round((this.earthAge * 11.86) * 100)/100;
     this.jupiterAge = newJupiterAge;
     return newJupiterAge;
+  }
+
+// Determine lifespan on other planets
+  findEarthLifeSpan() {
+    let newEarthLifeSpan;
+    /*istanbul ignore else*/
+    if (this.earthAge < 80) {
+      newEarthLifeSpan = (80 - this.earthAge); 
+      this.earthLifeSpan = newEarthLifeSpan;
+      return newEarthLifeSpan;
+    } else {
+      newEarthLifeSpan = (this.earthAge - 80); 
+      this.earthLifeSpan = newEarthLifeSpan;
+      return newEarthLifeSpan;
+    }
   }
 }
 
